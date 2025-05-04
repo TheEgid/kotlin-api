2fcd9a167f608fa99cc148c8c767658b87211f9d
TheEgid <egid.f1@gmail.com>
2025-05-04 10:52:35 +0300

    Rewrite structures

--- _scripts_powershell/copy_script.ps1
+++ _scripts_powershell/copy_script.ps1
@@ -10,14 +10,12 @@ $dst = [Environment]::GetFolderPath("Desktop") + "\" + $folderName + "\"
 
 $esx1 = $src + ".repomix"
 $esx2 = $src + ".git"
-$esx3 = $src + "app\build"
-$esx4 = $src + ".gradle"
-$esx5 = $src + ".kotlin"
+$esx3 = $src + "kotlin-api\app\build"
+$esx4 = $src + "kotlin-api\.gradle"
+$esx5 = $src + ".gradle"
+$esx6 = $src + "kotlin-api\.kotlin"
 
-robocopy $src $dst /MT:12 /MIR /XA:SH /XD $esx1 /XD $esx2 /XD $esx3 /XD $esx4 /XD $esx5 /XJD /NFL /NDL
-
-# $entrypointSh = $dst + "postgres-db\docker-entrypoint.sh"
-# (Get-Content $entrypointSh -Raw).Replace("`r`n", "`n") | Set-Content $entrypointSh -Force
+robocopy $src $dst /MT:12 /MIR /XA:SH /XD $esx1 /XD $esx2 /XD $esx3 /XD $esx4 /XD $esx5 /XD $esx6 /XJD /NFL /NDL
 
 $end = Get-Date
 $elapsed = $end - $start
